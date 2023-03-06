b8d7b3c89b8952ee7bf55f3ad9a992e9b8e1e77c
Ahmed Samir <107774249+Ahmedsamir28@users.noreply.github.com>
2023-03-06 02:41:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+import Items from "./components/Items";
+import ItemsDetails from "./components/ItemsDetails";
+import About from "./components/About";
+import Cart from "./components/Cart";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Navbar/>
+  <Routes>
+    <Route path="/" element = { <> <Landing/> <Items/> </>  } />
+
+    <Route path="about" element={<About/>}/>
+    <Route path="cart" element={<Cart/>}/>
+    <Route path="login" element={<Login/>}/>
+    <Route path="register" element={<Register/>}/>
+    <Route path="item/:productId" element={<ItemsDetails/>}/>
+  </Routes>
+  </>
   );
 }
 
